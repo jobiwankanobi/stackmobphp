b9c7f90747e60d1f6d330a9601160904eb42b1c5
jobiwankanobi <gymper@gmail.com>
2012-11-28 10:36:15 -0800

    Changed Stackmob::$VERSION to Stackmob::$DEVELOPMENT.

--- Stackmob/Stackmob.php
+++ Stackmob/Stackmob.php
@@ -12,10 +12,10 @@ class Stackmob {
 include("Rest.php");
 include("Object.php");
 include("User.php");
-include("Query.php");
+include("StackmobException.php");
 // include("Push.php");
 
 // Your credentials:
 Rest::$consumerKey = "1e2fc86e-50ad-4230-a30d-0ba86ee50fb0";
 Rest::$consumerSecret = "72b2fe32-1629-4684-88ec-05b5c29ea68d";
-Rest::$VERSION = 0;	// replace or override with 1 for live code
+Rest::$DEVELOPMENT = true;	// replace or override with false for live code
