082038225e769d8e6b27eb1810b04bf4ffe15c9c
jobiwankanobi <gymper@gmail.com>
2012-12-01 18:25:01 -0800

    reorganized project.

---
+++ src/Stackmob/Stackmob.php
@@ -0,0 +1,25 @@
+<?php
+namespace Stackmob;
+
+class Stackmob {
+
+    /**
+     * @var Rest
+     */
+    protected static $_restClient;
+}
+require 'vendor/autoload.php';
+\Logger::configure(__DIR__ . '/log4php.xml');
+
+include("Rest.php");
+include("Object.php");
+include("User.php");
+include("Query.php");
+include("CustomCode.php");
+include("StackmobException.php");
+// include("Push.php");
+
+// Your credentials:
+Rest::$consumerKey = "2423423423423423423";
+Rest::$consumerSecret = "23423432423424234234";
+Rest::$DEVELOPMENT = true;	// replace or override with false for live code
