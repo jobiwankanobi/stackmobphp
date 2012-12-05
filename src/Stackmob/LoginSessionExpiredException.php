fd09dadbd4a21e2567ef068a764da6d7f41c6331
jobiwankanobi <gymper@gmail.com>
2012-12-05 13:54:17 -0800

    initial commit.

---
+++ src/Stackmob/LoginSessionExpiredException.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of LoginSessionExpiredException
+ *
+ * @author jobrien
+ */
+namespace Stackmob;
+
+class LoginSessionExpiredException extends \Exception{
+    //put your code here
+}
+
+?>
