a9d49d7844cf80f6193167bf3be0e5af68a89752
jobiwankanobi <gymper@gmail.com>
2012-12-06 20:41:50 -0800

    Added back in tests.

--- tests/Stackmob/UserTest.php
+++ tests/Stackmob/UserTest.php
@@ -14,17 +14,17 @@ include_once(dirname(__FILE__) . "/../../src/Stackmob/Stackmob.php");
 class UserTest extends \PHPUnit_Framework_TestCase {
     
 
-//   public function testSignupUserStatic() {
-//        Rest::$consumerKey = "a368126f-4b41-4e54-ac45-394df81fe404";
-//        Rest::$consumerSecret = "1dd779df-ab0e-446b-8bdb-52641ef97df4";
-//
-//        $user = User::signUpUser("jimbo", "123456");
-//        $user->set("age", 25);
-//        $this->assertEquals("jimbo", $user->getUsername());
-//        $this->assertArrayHasKey("createddate", $user->attributes());
-//        $this->assertArrayHasKey("lastmoddate", $user->attributes());
-//        $this->assertArrayHasKey("sm_owner", $user->attributes());
-//    }
+   public function testSignupUserStatic() {
+        Rest::$consumerKey = "a368126f-4b41-4e54-ac45-394df81fe404";
+        Rest::$consumerSecret = "1dd779df-ab0e-446b-8bdb-52641ef97df4";
+
+        $user = User::signUpUser("jimbo", "123456");
+        $user->set("age", 25);
+        $this->assertEquals("jimbo", $user->getUsername());
+        $this->assertArrayHasKey("createddate", $user->attributes());
+        $this->assertArrayHasKey("lastmoddate", $user->attributes());
+        $this->assertArrayHasKey("sm_owner", $user->attributes());
+    }
 
      public function testLoginUserSuccess() {
         Rest::$consumerKey = "a368126f-4b41-4e54-ac45-394df81fe404";
@@ -49,47 +49,46 @@ class UserTest extends \PHPUnit_Framework_TestCase {
     }
 
     
-//    public function testDeleteUser() {
-//        $user = new User(array("username" => "jimbo"));
-//        $user->delete();
-//        try {
-//            $user->fetch();
-//        } catch(\Stackmob\StackmobException $e) {
-//            $this->assertEquals($e->getMessage(), "The requested URL returned error: 404");
-//        }
-//    }
+    public function testDeleteUser() {
+        $user = new User(array("username" => "jimbo"));
+        $user->delete();
+        try {
+            $user->fetch();
+        } catch(\Stackmob\StackmobException $e) {
+            $this->assertEquals($e->getMessage(), "The requested URL returned error: 404");
+        }
+    }
     
     /**
      * @expectedException \Stackmob\StackmobException
      */
-//    public function testLoginFailedException() {
-//
-//        $user = new User();
-//        $user->logIn("jimbo", "23423423423");   
-//    }
+    public function testLoginFailedException() {
+
+        $user = new User();
+        $user->logIn("jimbo", "23423423423");   
+    }
 
 
     
  
     
-//    public function testFetchUser() {
-//        $user = new User(array("username" => "jimbo"));
-//        $user->fetch();
-//        $this->assertArrayHasKey("createddate", $user->attributes());
-//        $this->assertArrayHasKey("lastmoddate", $user->attributes());
-//        $this->assertArrayHasKey("sm_owner", $user->attributes());
-//    }
-//    
+    public function testFetchUser() {
+        $user = new User(array("username" => "jimbo"));
+        $user->fetch();
+        $this->assertArrayHasKey("createddate", $user->attributes());
+        $this->assertArrayHasKey("lastmoddate", $user->attributes());
+        $this->assertArrayHasKey("sm_owner", $user->attributes());
+    }
 
     
-//    public function testSignupUserNonStatic() {
-//        $user = new User(array("username" => "jimbo", "password" => "123456"));
-//        $user->signUp();
-//        $this->assertArrayHasKey("createddate", $user->attributes());
-//        $this->assertArrayHasKey("lastmoddate", $user->attributes());
-//        $this->assertArrayHasKey("sm_owner", $user->attributes());
-//        $user->delete();
-//    }
+    public function testSignupUserNonStatic() {
+        $user = new User(array("username" => "jimbo", "password" => "123456"));
+        $user->signUp();
+        $this->assertArrayHasKey("createddate", $user->attributes());
+        $this->assertArrayHasKey("lastmoddate", $user->attributes());
+        $this->assertArrayHasKey("sm_owner", $user->attributes());
+        $user->delete();
+    }
 }
 
 ?>
