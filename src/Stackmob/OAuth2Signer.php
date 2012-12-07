24021e164c2c585f924d53e614f804d4de563ea4
jobiwankanobi <gymper@gmail.com>
2012-12-06 20:28:18 -0800

    Prepend / to path for it to work.

--- src/Stackmob/OAuth2Signer.php
+++ src/Stackmob/OAuth2Signer.php
@@ -89,26 +89,16 @@ class OAuth2Signer {
      * @return type
      */
     function generateMAC($method, $fullHost, $path) {
-        $this->log->debug("Fullhost: $fullHost");
+        $path = '/' . $path;
         $hostWithPort = preg_replace('/^http[s]?:\/\//', "", $fullHost);
-        $this->log->debug("HostWIthPort: $hostWithPort");
         $splitHost = split(':', $hostWithPort);
         $hostNoPort = count($splitHost) > 1 ? $splitHost[0] : $hostWithPort;
         $port = count($splitHost) > 1 ? $splitHost[1] : 80;  //use default port 80 if http.  If you're using https then this should be 443
-        $this->log->debug("HostWithPort: $hostWithPort");
-        $this->log->debug("Port: $port");
         $ts = \Stackmob\OAuthRequest::generate_timestamp();
-        $nonce = "n" . rand(0, 10000);
+        $nonce = substr(number_format(hexdec(sha1(microtime(true).mt_rand(10000,90000))),0,'',''), 0, 17);
         
         $base = $this->_createBaseString($ts, $nonce, $method, $path, $hostNoPort, $port);
-        $this->log->debug("BASE: $base");
-        $this->log->debug("Access token: " . $this->_accessToken);
-        $this->log->debug("Mac Key: " . $this->_macKey);
         $mac = \base64_encode(\hash_hmac('sha1', $base, $this->_macKey, true));
-//        $this->log->debug("BHMAC: $bhmac");
-//        $bstring = $this->_bin2String($bhmac);
-//        $this->log->debug("BSTRING: $bstring");
-//        $mac = base64_encode($bstring);
         $this->log->debug("HMAC: $mac");
         
         return 'Authorization:MAC id="' .  $this->_accessToken . '",ts="' . $ts . '",nonce="' . $nonce
