b78720725b5cd1110248f67c86cc12ca936ad1ed
jobiwankanobi <gymper@gmail.com>
2012-12-11 11:27:23 -0800

    added tests

--- tests/Stackmob/QueryTest.php
+++ tests/Stackmob/QueryTest.php
@@ -14,6 +14,7 @@ include_once(dirname(__FILE__) . "/../../src/Stackmob/Stackmob.php");
 class QueryTest extends \PHPUnit_Framework_TestCase {
    protected $log;
    protected $user;
+   protected $objects;
    
    public function setUp() {
        parent::setUp();
@@ -22,15 +23,41 @@ class QueryTest extends \PHPUnit_Framework_TestCase {
         $this->log = \Logger::getLogger(__CLASS__);
         $this->user = new User(array("username" => "jimbo", "password" => "123456", "age" => 25));
         $this->user->signUp();
+        
+        // Set up test objects
+        $this->objects = array();
+        $object1 = new Object('Flimmy', array('flimLevel' => 5,   // TODO: camel case should convert to underscore
+            'flimtackular' => false, 'flammy' => 'blammy'));
+        $object1->save();
+        $this->objects[] = $object1;
+        
+        $object2 = new Object('Flimmy', array('flimLevel' => 7, 'flimtackular' => true, 'flammy' => 'slammy'));
+        $object2->save();
+        $this->objects[] = $object1;
+        
+        
    }
    
    public function tearDown() {
        parent::tearDown();
        $this->user->delete();
+       foreach($this->objects as $object)
+           $object->delete();
+   }
+   
+   public function testObjectFetchWithEquals() {
+       $this->log->debug("testObjectFetchWithEquals");
+       $query = new Query('Flimmy');
+       $query->isEqual('flimLevel', 7);
+       
+       $results = $query->find();
+       $this->assertEquals(1, count($results));
+       $flim = $results[0];
+       $this->assertEquals('flimLevel', $flim->get('flimLevel'));
    }
 
-    public function testUserFetchWithAge() {
-        $this->log->debug("testUserFetchWithAge");
+    public function testUserFetchWithAgeEquals() {
+        $this->log->debug("testUserFetchWithAgeEquals");
         $query = new Query('User');
         $query->isEqual('age', 25);
         $results = $query->find();
@@ -44,6 +71,38 @@ class QueryTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals("25", $user->get("age"));
     }
     
+    public function testUserFetchWithAgeLessThan() {
+        $this->log->debug("testUserFetchWithAgeLessThan");
+        $query = new Query('User');
+        $query->lessThan('age', 26);
+        $results = $query->find();
+        $this->assertEquals(1, count($results));
+        $user = $results[0];
+        
+        $this->assertEquals("jimbo", $user->getUsername());
+        $this->assertArrayHasKey("createddate", $user->attributes());
+        $this->assertArrayHasKey("lastmoddate", $user->attributes());
+        $this->assertArrayHasKey("sm_owner", $user->attributes());
+        $this->assertEquals("25", $user->get("age"));
+        
+    }
+
+        public function testUserFetchWithAgeGreaterThan() {
+        $this->log->debug("testUserFetchWithAgeLessThan");
+        $query = new Query('User');
+        $query->greaterThan('age', 20);
+        $results = $query->find();
+        $this->assertEquals(1, count($results));
+        $user = $results[0];
+        
+        $this->assertEquals("jimbo", $user->getUsername());
+        $this->assertArrayHasKey("createddate", $user->attributes());
+        $this->assertArrayHasKey("lastmoddate", $user->attributes());
+        $this->assertArrayHasKey("sm_owner", $user->attributes());
+        $this->assertEquals("25", $user->get("age"));
+        
+    }
+
 }
 
 ?>
