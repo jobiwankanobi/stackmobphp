654256c01befa091b1b97970d0d65d231bfcad86
jobiwankanobi <gymper@gmail.com>
2012-12-29 12:43:47 -0800

    Make sure login, and added some tests

--- tests/Stackmob/QueryTest.php
+++ tests/Stackmob/QueryTest.php
@@ -13,51 +13,69 @@ include_once(dirname(__FILE__) . "/../../src/Stackmob/Stackmob.php");
 
 class QueryTest extends \PHPUnit_Framework_TestCase {
    protected $log;
-   protected $user;
-   protected $objects;
    
    public function setUp() {
        parent::setUp();
         Rest::$consumerKey = "a368126f-4b41-4e54-ac45-394df81fe404";
         Rest::$consumerSecret = "1dd779df-ab0e-446b-8bdb-52641ef97df4";
         $this->log = \Logger::getLogger(__CLASS__);
-        $this->user = new User(array("username" => "jimbo", "password" => "123456", "age" => 25));
-        $this->user->signUp();
+        $user = new User(array("username" => "jimbo", "password" => "123456", "age" => 25));
+        $user->signUp();
         
         // Set up test objects
-        $this->objects = array();
-        $object1 = new Object('Flimmy', array('flimLevel' => 5,   // TODO: camel case should convert to underscore
-            'flimtackular' => false, 'flammy' => 'blammy'));
+        
+        $object1 = new Object('Flimmy', array('flimlevel' => 5,   // TODO: camel case should convert to underscore
+            'flimtackular' => false));
         $object1->save();
-        $this->objects[] = $object1;
         
-        $object2 = new Object('Flimmy', array('flimLevel' => 7, 'flimtackular' => true, 'flammy' => 'slammy'));
+        $object2 = new Object('Flimmy', array('flimlevel' => 7, 'flimtackular' => true, 'flammy' => 'slammy'));
         $object2->save();
-        $this->objects[] = $object1;
-        
-        
    }
    
    public function tearDown() {
        parent::tearDown();
-       $this->user->delete();
-       foreach($this->objects as $object)
+       $user = new User(array("username" => "jimbo", "password" => "123456"));     
+       $user->logIn();
+       
+       $q = new Query("Flimmy");
+       $objects = $q->find();
+       foreach ($objects as $object) {
            $object->delete();
+       }
+       
+       $user->delete();
    }
    
    public function testObjectFetchWithEquals() {
        $this->log->debug("testObjectFetchWithEquals");
+       $user = new User(array("username" => "jimbo", "password" => "123456"));     
+       $user->logIn();
        $query = new Query('Flimmy');
-       $query->isEqual('flimLevel', 7);
+       $query->isEqual('flimlevel', 7);
        
        $results = $query->find();
        $this->assertEquals(1, count($results));
        $flim = $results[0];
-       $this->assertEquals('flimLevel', $flim->get('flimLevel'));
+       $this->assertEquals(7, $flim->get('flimlevel'));
+   }
+
+   public function testObjectFetchIsNull() {
+       $this->log->debug("testObjectFetchIsNull");
+       $user = new User(array("username" => "jimbo", "password" => "123456"));     
+       $user->logIn();
+       $query = new Query('Flimmy');
+       $query->isNull('flammy');
+       
+       $results = $query->find();
+       $this->assertEquals(1, count($results));
+       $flim = $results[0];
+       $this->assertEquals(7, $flim->get('flimlevel'));
    }
 
     public function testUserFetchWithAgeEquals() {
         $this->log->debug("testUserFetchWithAgeEquals");
+        $user = new User(array("username" => "jimbo", "password" => "123456"));     
+        $user->logIn();
         $query = new Query('User');
         $query->isEqual('age', 25);
         $results = $query->find();
@@ -73,6 +91,8 @@ class QueryTest extends \PHPUnit_Framework_TestCase {
     
     public function testUserFetchWithAgeLessThan() {
         $this->log->debug("testUserFetchWithAgeLessThan");
+        $user = new User(array("username" => "jimbo", "password" => "123456"));     
+        $user->logIn();
         $query = new Query('User');
         $query->lessThan('age', 26);
         $results = $query->find();
@@ -89,6 +109,8 @@ class QueryTest extends \PHPUnit_Framework_TestCase {
 
         public function testUserFetchWithAgeGreaterThan() {
         $this->log->debug("testUserFetchWithAgeLessThan");
+        $user = new User(array("username" => "jimbo", "password" => "123456"));     
+        $user->logIn();
         $query = new Query('User');
         $query->greaterThan('age', 20);
         $results = $query->find();
