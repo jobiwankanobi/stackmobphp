667b5c4663c07203e70ccc1f5d4d2384ef27cb2e
Iñaki Abete <inakiabt@gmail.com>
2013-09-24 03:22:51 -0500

    Add DummyLogger as default logger

--- src/Stackmob/Configuration.php
+++ src/Stackmob/Configuration.php
@@ -43,6 +43,10 @@ class Configuration
 
     public static function getLogger()
     {
+        if (self::$logger === NULL)
+        {
+            self::$logger = new DummyLogger();
+        }
         return self::$logger;
     }
 
